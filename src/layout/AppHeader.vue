a19f3efa7dbed817e65d692c331199dc6fc84f85
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2026-03-04 16:56:46 -0300

    first commit

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Bem-vindo à Calculadora de Gorjeta!</h1>
+    </header>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Calculadora from '../components/Calculadora.vue';
+import AppHeader from '../layout/AppHeader.vue';
+</script>
+
+<template>
+    <AppHeader/>
+     <Calculadora />
+</template>
